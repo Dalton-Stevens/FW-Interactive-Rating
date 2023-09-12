4d7e23e8f04336836fe5d6151bb2a4c93815a8ec
Dalton Stevens <d.72.stevens@gmail.com>
2023-09-12 15:27:22 -0500

    frontend mentor interactive rating

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import StarSvg from "../assets/icon-star.svg";
+
+const Card = () => {
+  return (
+    <div>
+      <img src={StarSvg} alt="Star SVG" />
+    </div>
+  );
+};
+
+export default Card;
